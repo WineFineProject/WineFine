cf0e0c4bcb8e97db60d7bbda670fe530d22183b1
sistt110 <sistt110@SIST110>
2024-09-30 16:01:13 +0900

    0930

---
+++ SpringBasicProject_DI_Test/src/main/java/com/sist/dao/GoodsDAO.java
@@ -0,0 +1,16 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.*;
+import com.sist.vo.*;
+@Repository("dao")
+public class GoodsDAO {
+	@Autowired
+    private GoodsMapper mapper;
+
+}
