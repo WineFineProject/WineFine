17eb283465d514fd613e960942a301b395d04741
user <user@DESKTOP-R8HLPOD>
2024-10-05 01:16:42 +0900

    InformationController 수정 (민선)

--- WineFineProject/src/main/java/com/sist/controller/InformationController.java
+++ WineFineProject/src/main/java/com/sist/controller/InformationController.java
@@ -1,11 +1,11 @@
 package com.sist.controller;
 import java.util.*;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sist.service.InformationService;
 import com.sist.vo.*;
@@ -25,7 +25,7 @@ public class InformationController {
 
   // 포도 상세 페이지
   @GetMapping("grape/detail.do")
-  public String grapeDetailData(@RequestParam("no") int no, Model model) 
+  public String grapeDetailData(@Param("no") int no, Model model) 
   {
       GrapeVO grape = iService.grapeDetailData(no);
       model.addAttribute("grape", grape);
@@ -43,7 +43,7 @@ public class InformationController {
 
   // 생산지역 상세 페이지
   @GetMapping("nation/detail.do")
-  public String nationDetailData(@RequestParam("no") int no, Model model) 
+  public String nationDetailData(@Param("no") int no, Model model) 
   {
       NationVO nation = iService.nationDetailData(no);
       model.addAttribute("nation", nation);
@@ -61,7 +61,7 @@ public class InformationController {
 
   // 생산자 상세 
   @GetMapping("maker/detail.do")
-  public String makerDetailData(@RequestParam("no") int no, Model model) 
+  public String makerDetailData(@Param("no") int no, Model model) 
   {
       MakerVO maker = iService.makerDetailData(no);
       model.addAttribute("maker", maker);
