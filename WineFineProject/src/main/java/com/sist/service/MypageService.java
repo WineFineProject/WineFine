0bad7d4bb5ed25ea57e256be354ac6171f0e74e7
user <user@DESKTOP-FEA4IDF>
2024-10-14 17:51:48 +0900

    --마이페이지 수정중(은영)

--- WineFineProject/src/main/java/com/sist/service/MypageService.java
+++ WineFineProject/src/main/java/com/sist/service/MypageService.java
@@ -6,12 +6,9 @@ import java.util.*;
 import com.sist.vo.BoardVO;
 
 public interface MypageService {
-	public List<BoardVO> getBoardsByUserId(String userId);	
-	int getboardCountByUserId(String userId);
-	public List<BoardVO> getBoardsByUserIdWithPaging(String userId,int limit,int offset);	
-	public List<Map<String, Object>> getBoardCountByCategoryForUser(String userId);
 
-	
+	public List<BoardVO> myboardListData(String nickname,int start,int end);
+	public int myPageBoardTotalPage(String nickname);
 //	 public List<BoardVO> myBoardListData(String nickname,int start,int end);
 //	 public List<BoardVO> boardTypeListData(String nickname,int type,int start,int end);
 //	 public int myPageBoardTotalPage(String nickname);
